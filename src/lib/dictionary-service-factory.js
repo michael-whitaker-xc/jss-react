22c690bc21a950edefb5e219ef53b3a0c6df9b32
Michael Whitaker <michael.whitaker@xcentium.com>
2022-06-01 09:49:42 -0500

    Initial

---
+++ src/lib/dictionary-service-factory.js
@@ -0,0 +1,13 @@
+import { RestDictionaryService } from '@sitecore-jss/sitecore-jss-react';
+import config from '../temp/config';
+export class DictionaryServiceFactory {
+  create() {
+    return new RestDictionaryService({
+      apiHost: config.sitecoreApiHost,
+      apiKey: config.sitecoreApiKey,
+      siteName: config.jssAppName,
+    });
+  }
+}
+
+export const dictionaryServiceFactory = new DictionaryServiceFactory();
